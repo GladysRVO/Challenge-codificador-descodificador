1ab75560fe66e161b98a67687c85b5e8b535a4ca
Gladys Vilchez <gladysrvo@gmail.com>
2024-03-05 09:43:30 -0600

    primer proyecto

---
+++ index.js
@@ -0,0 +1,48 @@
+const texto = document.querySelector(".texto");
+const mensaje = document.querySelector(".mensaje");
+
+function botonCodificar() {
+    const textoCodificado = codificar(texto.value);
+    mensaje.value = textoCodificado;
+    texto.value = "";
+}
+
+function botonDescodificar() {
+    const mensajeDescodificado = descodificar (mensaje.value);
+    texto.value = mensajeDescodificado;
+    mensaje.value = ""; 
+
+}
+
+function codificar (stringCondificada) {
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringCondificada = stringCondificada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringCondificada.includes(matrizCodigo[i][0])) {
+            stringCondificada = stringCondificada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+
+    return stringCondificada;
+}
+
+function descodificar (stringDescondificada) {
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDescondificada = stringDescondificada.toLowerCase();
+
+    for (let i = 1; i < matrizCodigo.length; i++) {
+        if (stringDescondificada.includes(matrizCodigo[i][1])) {
+            stringDescondificada = stringDescondificada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+    
+    return stringDescondificada;
+}
+
+document.getElementById('copiar');
+async function copiarContenido() {
+    let text = document.querySelector("#copiar").value;
+    await navigator.clipboard.writeText(mensaje);
+    alert("mensaje copiado")
+}
